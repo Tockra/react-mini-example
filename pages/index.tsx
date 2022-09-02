bd9a12765eaa77b2c8df0cc8412093366da5061d
Tim Tannert <tim.tannert@valtech.com>
2022-09-02 10:00:55 +0200

    With seperate

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,18 @@ import styles from "../styles/Home.module.css";
 import { DummyComp } from "../components/dummy";
 
 const Home = (_props: any) => {
-  const [myStateFromDb, setMyStateFromDb] = useState({
-    counter1: 0,
-    counter2: 0,
-  });
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
   return (
     <div>
       <h2>My Test</h2>
-      <DummyComp>{myStateFromDb.counter1}</DummyComp>
-      <DummyComp>{myStateFromDb.counter2}</DummyComp>
-      <button
-        onClick={() =>
-          setMyStateFromDb((oldState) => {
-            return { ...oldState, counter1: oldState.counter1 + 1 };
-          })
-        }
-      >
+      <DummyComp>{counter1}</DummyComp>
+      <DummyComp>{counter2}</DummyComp>
+      <button onClick={() => setCounter1((oldCount) => oldCount + 1)}>
         Increase 1
       </button>
-      <button
-        onClick={() =>
-          setMyStateFromDb((oldState) => {
-            return { ...oldState, counter2: oldState.counter2 + 1 };
-          })
-        }
-      >
+      <button onClick={() => setCounter2((oldCount) => oldCount + 1)}>
         Increase 2
       </button>
     </div>
